a9795f7120b3ccb8d96622565d98a4309ca6dff0
Alan Layng <alan.layng@gmail.com>
2026-05-01 18:41:07 +0100

    Make more class like and add gzip

--- src/index.js
+++ src/index.js
@@ -1,71 +1,138 @@
+import assert from 'node:assert'
 import { request as _request } from 'node:https'
+import { createGunzip } from 'node:zlib'
 import { globalAgent } from 'node:http'
 
-function demand (
-  method,
-  url,
-  {
-    redirect = true, //       redirect if necessary
-    body, //                  if given will be sent as the body
-    ...opts
-  } = {}
-) {
-  return new Promise((resolve, reject) => {
-    url = new URL(url)
-    opts.method ??= method
-    if (url.protocol === 'http:') opts.agent ??= globalAgent
+class Jeeves {
+  #res
+  #consumed = false
 
-    const req = _request(url, opts, res => {
-      res.text = text
-      res.json = json
-      if (res.statusCode >= 400) {
-        reject(makeError(res, url))
-      } else if (res.statusCode >= 300 && redirect && res.headers.location) {
-        url = new URL(res.headers.location, url)
-        demand(method, url, { body, ...opts }).then(resolve, reject)
-      } else {
-        resolve(res)
+  // -----------------------------------------------------------------
+  //
+  // Static level
+
+  static demand (
+    method,
+    url,
+    {
+      redirect = true, //       redirect if necessary
+      body, //                  if given will be sent as the body
+      ...opts
+    } = {}
+  ) {
+    return new Promise((resolve, reject) => {
+      url = new URL(url)
+      opts.method ??= method
+      if (url.protocol === 'http:') opts.agent ??= globalAgent
+
+      const req = _request(url, opts, res => {
+        if (res.statusCode >= 400) {
+          reject(this.#makeError(res, url))
+        } else if (res.statusCode >= 300 && redirect && res.headers.location) {
+          url = new URL(res.headers.location, url)
+          this.demand(method, url, { body, ...opts }).then(resolve, reject)
+        } else {
+          resolve(new Jeeves(res))
+        }
+      })
+
+      req.on('error', reject)
+
+      if (body) {
+        if (typeof body === 'object') {
+          if (Buffer.isBuffer(body)) {
+            // do nothing
+          } else if (body instanceof URLSearchParams) {
+            body = body.toString()
+            req.setHeader('Content-Type', 'application/x-www-form-urlencoded')
+          } else {
+            body = JSON.stringify(body)
+            req.setHeader('Content-Type', 'application/json')
+          }
+        }
+        req.setHeader('Content-Length', Buffer.byteLength(body))
+        req.write(body)
       }
+      req.end()
     })
-
-    req.on('error', reject)
-
-    if (body) {
-      if (typeof body === 'object' && !Buffer.isBuffer(body)) {
-        body = JSON.stringify(body)
-        req.setHeader('Content-Type', 'application/json')
-      }
-      req.setHeader('Content-Length', Buffer.byteLength(body))
-      req.write(body)
-    }
-    req.end()
-  })
-}
-
-function makeError (res, url) {
-  const err = new Error(res.statusMessage)
-  err.statusCode = res.statusCode
-  err.headers = res.headers
-  err.url = url + ''
-  return err
-}
-
-async function text () {
-  let data = ''
-  this.setEncoding('utf8')
-  for await (const chunk of this) {
-    data += chunk
   }
-  return data
+
+  static #makeError (res, url) {
+    const err = new Error(res.statusMessage)
+    err.statusCode = res.statusCode
+    err.headers = res.headers
+    err.url = url + ''
+    return err
+  }
+
+  // -----------------------------------------------------------------
+  //
+  // Instance level
+
+  constructor (res) {
+    this.#res = res
+    setImmediate(() => {
+      if (this.#consumed) return //
+      console.warn('Uncomsumed call to Jeeves')
+      this.resume()
+    })
+  }
+
+  get headers () {
+    return this.#res.headers
+  }
+
+  get statusCode () {
+    return this.#res.statusCode
+  }
+
+  resume () {
+    this.#consumed = true
+    // send the data into the void
+    this.#res.resume()
+  }
+
+  stream () {
+    assert(!this.#consumed, 'data has already been consumed')
+    this.#consumed = true
+    if (this.#res.headers['content-encoding'] === 'gzip') {
+      const gunzip = createGunzip()
+      gunzip.on('error', e => this.#res.emit('error', e))
+      return this.#res.pipe(gunzip)
+    }
+    return this.#res
+  }
+
+  async text () {
+    const str = this.stream()
+    str.setEncoding('utf8')
+    let data = ''
+    for await (const chunk of str) {
+      data += chunk
+    }
+    return data
+  }
+
+  async json (reviver) {
+    return JSON.parse(await this.text(), reviver)
+  }
+
+  async blob () {
+    const buffers = []
+    for await (const chunk of this.stream()) {
+      buffers.push(chunk)
+    }
+    return Buffer.concat(buffers)
+  }
+
+  [Symbol.asyncIterator] () {
+    return this.stream()[Symbol.asyncIterator]()
+  }
 }
 
-async function json (reviver) {
-  const text = await this.text()
-  return JSON.parse(text, reviver)
-}
-
-const get = demand.bind(null, 'GET')
-const post = demand.bind(null, 'POST')
+const get = Jeeves.demand.bind(Jeeves, 'GET')
+const post = Jeeves.demand.bind(Jeeves, 'POST')
+const demand = Jeeves.demand.bind(Jeeves)
 
 export { demand, get, post }
 export default { demand, get, post }
